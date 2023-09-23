89c6e0c0b5198eeb6abd07912a01e7dc4d3841a0
ariel-vaca <avaca@est.frba.utn.edu.ar>
2023-09-23 02:40:30 -0300

    .

--- tp_sintaxis/adf/main.c
+++ tp_sintaxis/adf/main.c
@@ -5,15 +5,20 @@
 
 int main()
 {
-    char cadena[20]= "-01230$-01231$1A";
+    char cadena[50]= "-01230$-01231$1A$012347$0xFFFF";
     //printf("respuesta %d",stringToInt("52322"));
 
     char * token = strtok(cadena, "$");
-
     while( token != NULL )
     {
-        int respuesta_evaluar_decimal = evaluar_decimal(token);
+      //  int respuesta_evaluar_decimal = evaluar_decimal(token);
+        //int respuesta_evaluar_octal = evaluar_octal(token);
+        int respuesta_evaluar_hexadecimal = evaluar_hexadecimal(token);
+
         token = strtok(NULL, "$");
+
+        printf("*************************************\n");
+
     }
     return 0;
 }
@@ -30,7 +35,7 @@ int evaluar_decimal(char *cadena)
     }
     else
     {
-        printf("No se evalua la palabra %s porque no verifica alfabeto\n", cadena);
+        printf("No se evalua la palabra %s porque no verifica alfabeto decimal\n", cadena);
     }
 }
 
@@ -89,5 +94,236 @@ int columna_decimal(int c)
         return 2;
     default:
         return 0;
+    }
+}
+/******************* OCTAL ************************/
+int evaluar_octal(char *cadena)
+{
+    int respuesta_verifica_alfabeto_octal = verifica_alfabeto_octal(cadena);
+    printf("verifica alfabeto octal %s? %d\n",cadena,respuesta_verifica_alfabeto_octal);
+
+    if(respuesta_verifica_alfabeto_octal==1)
+    {
+        printf("es palabra octal %s? %d\n",cadena,es_palabra_octal(cadena));
+
+    }
+    else
+    {
+        printf("No se evalua la palabra %s porque no verifica alfabeto octal\n", cadena);
+    }
+}
+
+int verifica_alfabeto_octal(char *s)
+{
+    unsigned int i;
+
+    for(i=0; s[i]; i++)
+    {
+        if(!(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int es_palabra_octal(char *cadena)
+{
+    const int FINAL_STATE = 1;
+    const int INVALID_STATE = 2;
+
+
+    const int tt[3][8]=
+    {
+        {FINAL_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE},
+        {FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE},
+        {INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE}
+    };
+
+    int estado = 0;
+    int i=0;
+    int c= cadena[i];
+    while(c!='\0')
+    {
+        estado=tt[estado][columna_octal(c)];
+        c = cadena[++i];
+    }
+
+    if(estado == FINAL_STATE)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int columna_octal(int c)
+{
+    switch(c)
+    {
+    case '0':
+        return 0;
+        break;
+    case '1':
+        return 1;
+        break;
+    case '2':
+        return 2;
+        break;
+    case '3':
+        return 3;
+        break;
+    case '4':
+        return 4;
+        break;
+    case '5':
+        return 5;
+        break;
+    case '6':
+        return 6;
+        break;
+    case '7':
+        return 7;
+        break;
+    }
+}
+
+/******************* HEXADECIMAL ************************/
+int evaluar_hexadecimal(char *cadena)
+{
+    int respuesta_verifica_alfabeto_hexadecimal = verifica_alfabeto_hexadecimal(cadena);
+    printf("verifica alfabeto hexadecimal %s? %d\n",cadena,respuesta_verifica_alfabeto_hexadecimal);
+
+    if(respuesta_verifica_alfabeto_hexadecimal==1)
+    {
+        printf("es palabra hexadecimal %s? %d\n",cadena,es_palabra_hexadecimal(cadena));
+
+    }
+    else
+    {
+        printf("No se evalua la palabra %s porque no verifica alfabeto hexadecimal\n", cadena);
+    }
+}
+
+int verifica_alfabeto_hexadecimal(char *s)
+{
+    unsigned int i;
+
+    for(i=0; s[i]; i++)
+    {
+        if(!(toupper(s[i])== 'A'||toupper(s[i])== 'B'||toupper(s[i])== 'C'||toupper(s[i])== 'D'||toupper(s[i])== 'E'||toupper(s[i])== 'F'||toupper(s[i])== 'X'||isdigit(s[i])))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int es_palabra_hexadecimal(char *cadena)
+{
+    const int FINAL_STATE = 2;
+    const int INVALID_STATE = 3;
+
+    const int tt[4][24]=
+    {
+        {INVALID_STATE,INVALID_STATE,1,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE},
+        {FINAL_STATE,FINAL_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE},
+        {INVALID_STATE,INVALID_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE,FINAL_STATE},
+        {INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE,INVALID_STATE},
+    };
+
+    int estado = 0;
+    int i=0;
+    int c= cadena[i];
+    while(c!='\0')
+    {
+        estado=tt[estado][columna_hexadecimal(c)];
+        c = cadena[++i];
+    }
+
+    if(estado == FINAL_STATE)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int columna_hexadecimal(int c)
+{
+    switch(c)
+    {
+    case 'x':
+        return 0;
+        break;
+    case 'X':
+        return 1;
+        break;
+    case '0':
+        return 2;
+        break;
+    case '1':
+        return 3;
+        break;
+    case '2':
+        return 4;
+        break;
+    case '3':
+        return 5;
+        break;
+    case '4':
+        return 6;
+        break;
+    case '5':
+        return 7;
+        break;
+    case '6':
+        return 8;
+        break;
+    case '7':
+        return 9;
+        break;
+    case '8':
+        return 10;
+        break;
+    case '9':
+        return 11;
+        break;
+    case 'a':
+        return 12;
+        break;
+    case 'A':
+        return 13;
+        break;
+    case 'b':
+        return 14;
+        break;
+    case 'B':
+        return 15;
+        break;
+    case 'c':
+        return 16;
+        break;
+    case 'C':
+        return 17;
+        break;
+    case 'd':
+        return 18;
+        break;
+    case 'D':
+        return 19;
+        break;
+    case 'e':
+        return 20;
+        break;
+    case 'E':
+        return 21;
+        break;
+    case 'f':
+        return 22;
+        break;
+    case 'F':
+        return 23;
+        break;
     }
 }
