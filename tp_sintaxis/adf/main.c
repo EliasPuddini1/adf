d605054fc8e0e4c5c471d99bbbfcd0cb06a51265
ariel-vaca <avaca@est.frba.utn.edu.ar>
2023-09-22 23:29:28 -0300

    adf decimal

--- tp_sintaxis/adf/main.c
+++ tp_sintaxis/adf/main.c
@@ -1,9 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "string_utils.h"
 
 int main()
 {
-    printf("respuesta %ld",str_to_int("52322"));
+    char s[20]= "-01230";
+    //printf("respuesta %d",stringToInt("52322"));
+    printf("verifica alfabeto decimal? %d\n",verifica_decimal(s));
+
+    printf("es palabra decimal? %d\n",es_palabra_decimal(s));
+
     return 0;
+}
+
+int verifica_decimal(char *s)
+{
+    unsigned int i;
+
+    for(i=0; s[i]; i++)
+    {
+        if(!(s[i]=='+'|| s[i]=='-'||isdigit(s[i])))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int es_palabra_decimal(char *cadena)
+{
+
+    const int tt[4][3]=
+    {
+        {2,1,1},
+        {2,3,3},
+        {2,3,3},
+        {3,3,3}
+    };
+
+    int estado = 0;
+    int i=0;
+    int c= cadena[i];
+    while(c!='\0')
+    {
+        estado=tt[estado][columna(c)];
+        c = cadena[++i];
+    }
+
+    if(estado == 2)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int columna(int c)
+{
+    switch(c)
+    {
+    case '+':
+        return 1;
+    case '-':
+        return 2;
+    default:
+        return 0;
+    }
 }
--- tp_sintaxis/adf/string_utils.h
+++ tp_sintaxis/adf/string_utils.h
@@ -1,6 +1,23 @@
 #include <stdlib.h>
 
-int str_to_int(char* cadena)
+/*int str_to_int(char* cadena)
 {
     return (int)strtol(cadena,NULL, 10);
+}*/
+
+int stringToInt(const char *str) {
+    int result = 0;
+    int signo = 1;
+
+    if (*str == '-') {
+        signo = -1;
+        str++;
+    }
+
+    while (*str != '\0') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+
+    return signo * result;
 }
