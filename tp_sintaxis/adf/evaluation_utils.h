c7b8c2b427da3145108896011a7d79f9bae1c12e
ariel-vaca <avaca@est.frba.utn.edu.ar>
2023-09-25 17:59:21 -0300

    .

--- tp_sintaxis/adf/evaluation_utils.h
+++ tp_sintaxis/adf/evaluation_utils.h
@@ -66,12 +66,52 @@ int columna_decimal(int c)
 {
     switch(c)
     {
-    case '+':
-        return 1;
-    case '-':
-        return 2;
-    default:
+    case '0':
         return 0;
+        break;
+    case '1':
+        return 1;
+        break;
+
+    case '2':
+        return 2;
+        break;
+
+    case '3':
+        return 3;
+        break;
+
+    case '4':
+        return 4;
+        break;
+
+    case '5':
+        return 5;
+        break;
+
+    case '6':
+        return 6;
+        break;
+
+    case '7':
+        return 7;
+        break;
+
+    case '8':
+        return 8;
+        break;
+
+    case '9':
+        return 9;
+        break;
+
+    case '+':
+        return 10;
+        break;
+
+    case '-':
+        return 11;
+        break;
     }
 }
 /******************* OCTAL ************************/
