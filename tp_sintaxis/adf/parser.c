f10f88a6870241a16e8304852a28921faae1b401
EliasPuddini1 <epuddini@frba.utn.edu.ar>
2023-09-30 12:01:33 -0300

    Final

--- tp_sintaxis/adf/parser.c
+++ tp_sintaxis/adf/parser.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int evaluarExpresionRPN(const char *expresionRPN) {
     int stack[100]; // Pila para almacenar operandos
     int top = -1;   // Índice del elemento superior de la pila
+
 
     for (int i = 0; expresionRPN[i] != '\0'; i++) {
         char caracter = expresionRPN[i];
@@ -46,17 +48,64 @@ int evaluarExpresionRPN(const char *expresionRPN) {
     return stack[top];
 }
 
+// Función para determinar la prioridad de los operadores
+int getPriority(char operador) {
+    if (operador == '+' || operador == '-') {
+        return 1;
+    } else if (operador == '*' || operador == '/') {
+        return 2;
+    }
+    return 0; // Operador no reconocido
+}
+
+// Función para convertir expresión infija a posfija sin paréntesis
+void infixToPostfixNoParentheses(const char *expresionInfix, char *expresionPostfix) {
+    int stack[100];
+    int top = -1;
+    int outputIndex = 0;
+
+    for (int i = 0; expresionInfix[i] != '\0'; i++) {
+        char caracter = expresionInfix[i];
+
+        if (isdigit(caracter)) {
+            // Si es un dígito, agregarlo directamente a la salida
+            while (isdigit(expresionInfix[i])) {
+                expresionPostfix[outputIndex++] = expresionInfix[i++];
+            }
+            expresionPostfix[outputIndex++] = ' '; // Separador
+            i--; // Retroceder un paso para compensar el incremento en el bucle for
+        } else if (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/') {
+            // Si es un operador, desapilar operadores de mayor o igual prioridad y agregarlos a la salida
+            while (top >= 0 && getPriority(stack[top]) >= getPriority(caracter)) {
+                expresionPostfix[outputIndex++] = stack[top--];
+                expresionPostfix[outputIndex++] = ' '; // Separador
+            }
+            stack[++top] = caracter; // Apilar el operador actual
+        }
+    }
+
+    // Desapilar cualquier operador restante en la pila
+    while (top >= 0) {
+        expresionPostfix[outputIndex++] = stack[top--];
+        expresionPostfix[outputIndex++] = ' '; // Separador
+    }
+
+    expresionPostfix[outputIndex] = '\0'; // Terminar la cadena
+}
 
 int main() {
 
+    char expresionInfix[100];
+    char expresionPostfix[100];
 
-    char expresionRPN[100];
+    printf("Ingrese una expresión aritmética en notación infija sin paréntesis: ");
+    fgets(expresionInfix, sizeof(expresionInfix), stdin);
+    expresionInfix[strlen(expresionInfix) - 1] = '\0'; // Eliminar el salto de línea al final
 
-    printf("Ingrese una expresión aritmética en RPN: ");
-    fgets(expresionRPN, sizeof(expresionRPN), stdin);
-    expresionRPN[strlen(expresionRPN) - 1] = '\0'; // Eliminar el salto de línea al final
+    infixToPostfixNoParentheses(expresionInfix, expresionPostfix);
+    printf("Expresión en notación posfija: %s\n", expresionPostfix);
 
-    int resultado = evaluarExpresionRPN(expresionRPN);
+    int resultado = evaluarExpresionRPN(expresionPostfix);
     printf("Resultado: %d\n", resultado);
 
     return 0;
